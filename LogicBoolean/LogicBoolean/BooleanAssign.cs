fc04a0bba1d2949668e71f0541cf04c59a4f2438
moise7 <63320773+moise7@users.noreply.github.com>
2020-06-30 17:19:52 -0400

    Add files via upload

---
+++ LogicBoolean/LogicBoolean/BooleanAssign.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicBoolean
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            Console.WriteLine("What is your age?");
+            
+            age = Convert.ToInt32(Console.ReadLine());
+
+            if (age < 15)
+            {
+                Console.Write("Sorry, You are not qualifies for car insurance.\n");
+                Console.Write("You need to be over 15 year.\n\n", 18 - age);
+            }
+            else
+                Console.WriteLine("Congratulation! You are qualifies for a car insurance. Process!!\n\n");
+            Console.ReadLine();
+            
+
+            Console.WriteLine("Have you ever had a DUI?");
+            Console.ReadLine();
+            bool condition = true;
+            if (condition = true)
+            {
+                Console.WriteLine("Process....\n");
+
+            }
+            else {
+                Console.WriteLine("Try again!!!\n");            
+            }
+            
+            Console.WriteLine("How many speeding tickets do you have?");
+            Console.ReadLine();
+            int DUI;
+
+            Console.WriteLine("Have you ever had a DUI?");
+            DUI = Convert.ToInt32(Console.ReadLine());
+            if (DUI < 0)
+            {
+                Console.WriteLine("You Qualified. \n");
+            }
+            else
+                Console.WriteLine("You can't proceed, try again!!!");
+            Console.WriteLine("You selected" + age + "as your age");
+
+
+        }
+    }
+}
