400cbd62bf3bdc53d62a196823d135bece8a36e0
moise7 <63320773+moise7@users.noreply.github.com>
2020-07-01 00:12:50 -0400

    Add files via upload

---
+++ LoopAssigment/LoopAssigment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopAssigment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Do a boolean comparison using a while statement
+            int i = 0;
+            while (i < 10)
+            {
+                Console.WriteLine("i= {0}",i);
+                i++;
+                Console.ReadLine();
+                
+
+            }
+            
+            //Do a boolean comparison using a do while statement
+            int m = 0;
+            do
+            {
+                Console.WriteLine("m ={0}", m);
+                i++;
+            }
+            while (m > 20);
+        }
+    }
+}
