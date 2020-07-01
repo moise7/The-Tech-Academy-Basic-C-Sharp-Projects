87d7650a4b8f0935e248a09d7c834cd3d687e6eb
moise7 <63320773+moise7@users.noreply.github.com>
2020-07-01 01:44:44 -0400

    Add files via upload

---
+++ Arrayandlist/Arrayandlist/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //Create a one-dimensional Array of strings
+        string[] nameArray = new string[] { "Moise", "Kristin", "Paul" };
+            string index = Console.ReadLine();
+            Console.WriteLine("Select a name "+nameArray[index]);
+
+        //Create a one-dimensional Array of integer
+        int[] numArray = new int[] {5,10,25,30,100,100};
+        int index = Console.ReadLine();
+        Console.WriteLine("Select a number " + numArray[index]);
+
+        List<string> intList = new List<string>();
+        intList.Add("I love music");
+        intList.Add("I love cars");
+        intList.Remove("I like to Look good");
+        Console.WriteLine("Select a sentence "+intList[0]);
+        Console.ReadLine();
+    }
+    }
+
