d37a4ddf730514408d6774422f20a491c3ae3875
moise7 <63320773+moise7@users.noreply.github.com>
2020-07-05 18:36:09 -0400

    Add files via upload

--- BranchingAssignment/BranchingAssignment/Program.cs
+++ BranchingAssignment/BranchingAssignment/Program.cs
@@ -15,32 +15,41 @@ namespace BranchingAssignment
             Console.ReadLine();
             Console.WriteLine("Please enter the package weight:");
             Console.ReadLine();
-            int Weight = 50;
+            int Weight=50;
+
 
             if (Weight > 50)
             {
-                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
-               
+
+                Console.WriteLine("Sorry, package too heavy to be shipped via Package Express. Have a good day.");
+
+
 
             }
             else
             {
+
                 Console.WriteLine("Please enter the package width: ");
                 int width = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("Please enter the package height: ");
                 int height = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("Please enter the package length: ");
                 int length = Convert.ToInt32(Console.ReadLine());
-                int result = length + width + height;
+                int dimension = height + width + length;
+                int result = length + width + height * Weight / 100;
                 Console.WriteLine("Your estimated total for shipping this package is: " + result);
+                Console.ReadLine();
+
 
             }
-            Console.ReadLine();
-            
-           
 
-
-            
         }
+
+
+
+
+
+
     }
-}
+        }
+    
