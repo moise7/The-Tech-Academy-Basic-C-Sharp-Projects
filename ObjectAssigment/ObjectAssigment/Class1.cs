594abc69aacbe68fda1d1c662b18a68aa43c3258
moise7 <63320773+moise7@users.noreply.github.com>
2020-07-08 17:34:43 -0400

    Add files via upload

---
+++ ObjectAssigment/ObjectAssigment/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectAssigment
+{
+    public class Class1
+    {
+        public int method(int num1, int num2)
+        {
+            int result = num1 - num2;
+
+            return result;
+        }
+        public int methodOne(int num3, int num4)
+        {
+            int result = num3 * num4;
+            return result;
+
+        }
+        public int last(int num5, int num6)
+        {
+            num5 = 10;
+            int result = num5 + num6;
+            return result;
+        }
+    }
+}
---
+++ ObjectAssigment/ObjectAssigment/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectAssigment
+{
+   public class Program
+    {
+      public  static void Main(string[] args)
+
+        {
+            // part 1
+            Console.WriteLine("Enter a number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Class1 class1 = new Class1();
+            int result;
+            result = class1.method(num1, num2);
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+            //part 2
+            Console.WriteLine("Enter a decimal number ");
+            int num3 = (int)Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Enter the second number ");
+            int num4 = Convert.ToInt32(Console.ReadLine());
+            Class1 class1 = new Class1();
+            int result;
+            result = class1.methodOne(num3, num4);
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+            //part3
+
+            Console.WriteLine("Enter a number");
+            
+            int num5 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number ")
+            int num6 = Convert.ToInt32(Console.ReadLine());
+            Class1 class1 = new Class1();
+            int result;
+            result = class1.last(num5, num6);
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
