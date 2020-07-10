3b632748f110cb1cd490514996484e9a2a0890e0
moise7 <63320773+moise7@users.noreply.github.com>
2020-07-10 00:34:52 -0400

    Add files via upload

---
+++ ObjectAssigment1/ObjectAssigment1/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectAssigment1
+{
+    public class Class1
+    {
+        public static int math(int num1, int num2 = 1)
+        {
+            int result = num1 - num2;
+
+            return result;
+        }
+       
+    }
+}
---
+++ ObjectAssigment1/ObjectAssigment1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectAssigment1
+{
+    public class Program
+    {
+       public static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter a number ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+           
+            Console.WriteLine("You don't have to enter the second number " );
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.ReadLine();
+            Class1 class1 = new Class1();
+          int result;
+            if (num2 == 1)
+            {
+                Class1.math(num1);
+            }
+            else 
+            {
+                Class1.math(num1, num2);
+            }
+            result = Class1.math(num1, num2);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+            
+        }
+    }
+}
