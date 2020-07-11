07f7824189db793592915e9d8a0fa8cf61e1507d
moise7 <63320773+moise7@users.noreply.github.com>
2020-07-11 00:28:50 -0400

    Add files via upload

---
+++ ObjectAssigment2/ObjectAssigment2/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectAssigment2
+{
+    public class Class1
+    {
+       public static void Math(int num1, int num2)
+        {
+            int result = num1 * num2;
+            return;
+            
+        }
+    }
+}
---
+++ ObjectAssigment2/ObjectAssigment2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectAssigment2
+{
+    class Program
+    {//165
+      public  static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Class1 Math = new Class1();
+            int result;
+           Console.WriteLine("you have enter"+  num1);
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            
+
+           // result = Class1.Math(int num1, int num2);
+        }
+    }
+}
