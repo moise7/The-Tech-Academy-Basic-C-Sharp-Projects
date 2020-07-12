6d77166181eda3ae7c56b7e4d3a5017f88106266
moise7 <63320773+moise7@users.noreply.github.com>
2020-07-12 13:57:00 -0400

    Add files via upload

---
+++ Structs/Structs/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number amount = new Number();
+            Int32 num = 100;
+            Console.WriteLine(num);
+            Console.ReadLine();
+        }
+    }
+}
