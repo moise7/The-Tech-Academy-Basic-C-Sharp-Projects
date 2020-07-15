d32e025cc654af7da8a21453e4ff2dfdaccb461f
moise7 <63320773+moise7@users.noreply.github.com>
2020-07-14 23:34:18 -0400

    Add files via upload

---
+++ FileIO/FileIO/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace FileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            string a = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\Moise\Logs\log.text",a);
+
+                    
+        }
+    }
+}
