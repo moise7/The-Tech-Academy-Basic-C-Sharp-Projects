75f3af22ba867d6e060cf61844dcf9cf6d5604da
moise7 <63320773+moise7@users.noreply.github.com>
2020-07-15 02:10:14 -0400

    Add files via upload

---
+++ Assigment/Assigment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assigment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating const variable
+            const int b = 10;
+            var name = "Moise";
+            
+            
+        }
+        public Program(int b) : this() { }
+
+        public Program()
+        {
+        }
+    }
+}
