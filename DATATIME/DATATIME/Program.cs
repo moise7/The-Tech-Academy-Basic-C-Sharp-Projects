1c8b1f2984028173002601dd1a7db1faddef8195
moise7 <63320773+moise7@users.noreply.github.com>
2020-07-15 00:12:00 -0400

    Add files via upload

---
+++ DATATIME/DATATIME/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DATATIME
+{
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now);
+            Console.ReadLine();
+
+            Console.WriteLine("Enter a number");
+            string a = Console.ReadLine();
+            Console.WriteLine(a, now);
+
+        }
+    }
+}
