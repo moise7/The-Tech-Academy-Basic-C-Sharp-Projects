6757e7b16a67fc13a093020012db39074c0d7918
moise7 <63320773+moise7@users.noreply.github.com>
2020-07-22 06:01:42 -0400

    Add files via upload

--- AgeAssign/AgeAssign/Program.cs
+++ AgeAssign/AgeAssign/Program.cs
@@ -11,28 +11,30 @@ namespace AgeAssign
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("How old are you: ");
+            try
+            {
+                Console.WriteLine("How old are you: ");
             int a = Convert.ToInt32(Console.ReadLine());
 
             DateTime currentDate = DateTime.Now;
             DateTime futureDate = currentDate.AddYears(-a);
 
-            a = a * -1;
             if (a <= 0)
             {
                 throw new Exception();
-                Console.WriteLine("Your age can't be 0 try again");
+                
             }
-            try
-            {
+           
 
                 Console.WriteLine("You were born in: " + futureDate.Year);
-                Console.ReadLine();
+                
             }
             catch (Exception)
             {
                 Console.WriteLine("An error occurred. Please contact out support center");
+         
             }
+            Console.ReadLine();
         }
     }
 }
