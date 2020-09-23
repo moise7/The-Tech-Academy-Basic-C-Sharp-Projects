0ad5b70f0b9aa7c43f1326f22ca215e1cee68812
moise7 <63320773+moise7@users.noreply.github.com>
2020-09-23 17:13:56 -0400

    Add files via upload

--- Assignment1to7/Assignment1to7/Program.cs
+++ Assignment1to7/Assignment1to7/Program.cs
@@ -8,29 +8,30 @@ namespace Assignment1to7
         static void Main(string[] args)
         {
             //creating a one-dimensional array of strings
-            string[] movies = {"black panther", "Avengers", "X-man"};
-               
-            Console.WriteLine("type a text");
-            //int i = Convert.ToInt32(Console.ReadLine());
-            foreach(var item in movies)
+            string[] movies = { "black panther", "Avengers", "X-man" };
+
+           Console.WriteLine("type a text");
+            int i = Convert.ToInt32(Console.ReadLine());
+            foreach (var item in movies)
             {
-                Console.WriteLine(item);
-                
+                Console.WriteLine(item+i);
+                Console.ReadLine();
             }
             //creating an infinite loop
-            for(int a = 0; 0<50; a--)
+           // for (int a = 0; 0 < 50; a++)
+          //  {
+            //    Console.WriteLine("value:{0}", a);
+                
+           // }
+            //A loop where the comparison that’s used to determine whether to continue iterating the loop is a “<” operator
+            for(int j= 0; j <movies.Length; j++)
             {
-                Console.WriteLine("value:{0}", a);
+                Console.WriteLine(movies[j]);
             }
-            //fixing the infinte loop
-            for(int a = 0; 0 < 50; a++)
-            {
-                Console.WriteLine("value:{0}", a);
-            }
-            Console.ReadLine();
+          
         }
-        
-       
-       
+
+
+
     }
 }
