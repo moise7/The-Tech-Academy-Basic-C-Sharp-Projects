89bfd68f232786eb6f3b56ee0909baf8934be46a
moise7 <63320773+moise7@users.noreply.github.com>
2020-09-28 19:35:13 -0400

    Add files via upload

--- Assignment1to7/Assignment1to7/Program.cs
+++ Assignment1to7/Assignment1to7/Program.cs
@@ -10,19 +10,24 @@ namespace Assignment1to7
             //creating a one-dimensional array of strings
             string[] movies = { "black panther", "Avengers", "X-man" };
 
-            Console.WriteLine("type a text");
-            Console.ReadLine();
+            Console.WriteLine("type a text ");
+            var i = Console.ReadLine();
             foreach (var item in movies)
             {
-                Console.WriteLine(item+movies);
-                Console.ReadLine();
+                Console.WriteLine(item +  i);
+
             }
             //creating an infinite loop
-            // for (int a = 0; 0 < 50; a++)
-            //  {
+           // for (int a = 0; 0 < 50; a++)
             //    Console.WriteLine("value:{0}", a);
 
             // }
+            //fixing loop
+            for (int a = 0; a< 50; a++)
+            {
+                Console.WriteLine("value:{0}", a);
+
+            }
             //A loop where the comparison that’s used to determine whether to continue iterating the loop is a “<” operator
             for (int j = 0; j < movies.Length; j++)
             {
@@ -38,12 +43,16 @@ namespace Assignment1to7
                 // A list of strings where each item in the list is unique
                 List<string> names = new List<string>() { "Moise", "Erik", "Adam", "Daniel" };
                 Console.WriteLine("Type a text here..");
-                Console.ReadLine();
+                var a = Console.ReadLine();
                 foreach (string name in names)
                 {
-                    if (name == "Moise")
+                    if (name == a)
                     {
-                        Console.WriteLine(name);
+                        Console.WriteLine(name);break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("No in the List try again");
                     }
                 }
             }
@@ -52,6 +61,38 @@ namespace Assignment1to7
             {
                 Console.WriteLine(ex.Message);
             }
+            //Create a list of strings that has at least two identical strings in the list
+            List<string> nicknames = new List<string>() { "Nick", "MB", "momo", "MB", "momo", "lukage", "Louis" };
+            Console.WriteLine("Type a nicknames");
+            string userNicks = Console.ReadLine();
+            foreach (string nickname in nicknames)
+            {
+                if (nickname == userNicks)
+                {
+                    Console.WriteLine(nickname);
+                    Console.ReadLine();
+                }
+                else 
+                {
+                    Console.WriteLine("Try again");
+                }
+            }
+            //Create a list of strings that has at least two identical strings in the list
+            List<string> states = new List<string>() { "Indiana", "Chicago", "DC", "New York", "DC", "Atlanta" };
+            
+            //Create a foreach loop that evaluates each item in the list, and displays a message showing the string and whether or not it has already appeared in the list
+            foreach (string state in states)
+            {
+                for (int i = 0; i < states.IndexOf(state); i++)
+                {
+                    if (state == states[i])
+                    {
+                        Console.WriteLine(state + " has appeared before in this list.");
+                    }
+                }
+            }
         }
+
+
     }
 }
