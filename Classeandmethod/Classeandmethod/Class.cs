4f8c1675b93db76b52922390bb2e5ef7995e4abc
moise7 <63320773+moise7@users.noreply.github.com>
2020-09-28 22:50:52 -0400

    Add files via upload

---
+++ Classeandmethod/Classeandmethod/Class.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classeandmethod
+{
+    class Class
+    {
+        public static Class (int a, int b)
+        {
+            Console.WriteLine(a + b);
+            int i = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
---
+++ Classeandmethod/Classeandmethod/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Classeandmethod
+{
+    public class Program
+    {
+      public static void Main(string[] args)
+        {
+            Class class = new Class();
+            int result;
+            result = class.methodOne(a+b);
+            Console.WriteLine(result);
+        }
+    }
+}
