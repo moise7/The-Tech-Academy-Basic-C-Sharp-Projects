4f8d4751d1d7172e4caad5188ccb54547366ccc5
moise7 <63320773+moise7@users.noreply.github.com>
2020-09-29 21:19:18 -0400

    Add files via upload

--- ExceptionAssigment/ExceptionAssigment/Program.cs
+++ ExceptionAssigment/ExceptionAssigment/Program.cs
@@ -11,22 +11,38 @@ namespace ExceptionAssigment
         static void Main(string[] args)
         {
             try
-            { 
+            {
                 int[] numArray = { 23, 56, 78, 67, 45, 88 };
                 Console.WriteLine("Enter a number: ");
                 int num = Convert.ToInt32(Console.ReadLine());
                 int result;
-                foreach(int item in numArray)
-                    {
-                 Console.WriteLine(result = item / num);
-                Console.ReadLine();
-                    }
+                if (num < 1)
+                {
+                    throw new FormatException();
+                    
+                }
+              
+               foreach (int item in numArray)
+                {
+                   
+                    Console.WriteLine(result = item / num);
+                    Console.ReadLine();
+                }
+               
             }
-            catch(FormatException ex)
+            catch (FormatException ex)
             {
-                Console.WriteLine("Please enter a different number");
-                Console.ReadLine();
+                {
+                    Console.WriteLine("The number cannot be less than 1");
+                    Console.ReadLine();
+
+                    Console.WriteLine("The number cannot be negative");
+                    Console.ReadLine();
+
+                }
+               
+
             }
-    }
+        }
     }
 }
