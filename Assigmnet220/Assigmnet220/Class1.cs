c70356db89d81143d8e214b3c75958c3bf8f3764
moise7 <63320773+moise7@users.noreply.github.com>
2020-09-29 22:20:35 -0400

    Add files via upload

---
+++ Assigmnet220/Assigmnet220/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assigmnet220
+{
+    public class Class1
+    {
+        public int method(int a, int b = 20)
+        {
+            int result = a + b;
+            return result;
+        }
+    }
+}
---
+++ Assigmnet220/Assigmnet220/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Assigmnet220
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 ClassObject = new Class1();
+            Console.WriteLine("enter your first number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Leave this blank");
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+            
+            int result = ClassObject.method(num1);
+            Console.WriteLine("Total\t\t{0} ",  result);
+            Console.ReadLine(); 
+            
+        }
+    }
+}
