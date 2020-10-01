07f2646629765b755f5504ce754075d125c01482
moise7 <63320773+moise7@users.noreply.github.com>
2020-10-01 16:09:44 -0400

    Add files via upload

---
+++ Assigment 224/Assigment 224/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assigment_224
+{
+    public static class Class1
+    {
+        public void methodInt(int a = 3, int b = 6)
+        {
+            int sum = a + b / 2;
+            return sum;
+
+        }
+        public void Value(out int a, out int b)
+        {
+
+        }
+    }
+}
---
+++ Assigment 224/Assigment 224/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Assigment_224
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 Objectmethod = new Class1();
+            Console.WriteLine("Enter a number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int sum = Objectmethod.method(num1);
+            
+        }
+    }
+}
