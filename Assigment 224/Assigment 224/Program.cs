b77533a828f8826664a6575b663eb994fd312696
moise7 <63320773+moise7@users.noreply.github.com>
2020-10-05 22:44:26 -0400

    Add files via upload

--- Assigment 224/Assigment 224/Program.cs
+++ Assigment 224/Assigment 224/Program.cs
@@ -6,10 +6,10 @@ namespace Assigment_224
     {
         static void Main(string[] args)
         {
-            Class1 Objectmethod = new Class1();
+            
             Console.WriteLine("Enter a number");
             int num1 = Convert.ToInt32(Console.ReadLine());
-            int sum = Objectmethod.methodInt(num1);
+            int sum = Class1.methodInt(num1);
             
         }
     }
