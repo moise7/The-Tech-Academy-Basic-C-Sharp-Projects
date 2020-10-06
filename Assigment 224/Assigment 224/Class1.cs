7a103269fa4b34dd91c65aa6346a83a2632f5489
moise7 <63320773+moise7@users.noreply.github.com>
2020-10-05 23:36:53 -0400

    Add files via upload

--- Assigment 224/Assigment 224/Class1.cs
+++ Assigment 224/Assigment 224/Class1.cs
@@ -4,17 +4,24 @@ using System.Text;
 
 namespace Assigment_224
 {
-    public  class Class1
+    public static class Class1
     {
-        public void methodInt(int a, int b)
+        public static void methodInt(int a)
         {
-            int sum = a + b / 2;
+            int sum = a / 2;
             Console.WriteLine(sum);
 
         }
         public static int Value(out int a, out int b)
         {
-
+            a = 7;
+            b = 12;
+            return a * b;
+        }
+        public static double Value(double x, double y)
+        {
+            return x + y;
+            
         }
     }
 }
