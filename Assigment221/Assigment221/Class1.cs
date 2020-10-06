071ce76ab271cb4e99be17065ad7ff0b8e7da744
moise7 <63320773+moise7@users.noreply.github.com>
2020-10-06 18:51:25 -0400

    Add files via upload

---
+++ Assigment221/Assigment221/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assigment221
+{
+    public class Class1
+    {
+        public static void method(int a, int b)
+        {
+            int sum = a / b;
+            Console.WriteLine("The sum of {0} +{1}={2}", a, b, sum);
+        }
+    }
+}
---
+++ Assigment221/Assigment221/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Assigment221
+{
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+            Class1 ClassObject = new Class1();
+            int sum = ClassObject.method(1, 2);
+        }
+    }
+}
