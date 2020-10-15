7432339949d0c9a0c3e274892ccdb3760f40e182
moise7 <63320773+moise7@users.noreply.github.com>
2020-10-15 17:54:41 -0400

    Add files via upload

--- Abstract/Abstract/Employee.cs
+++ Abstract/Abstract/Employee.cs
@@ -6,27 +6,27 @@ using System.Threading.Tasks;
 
 namespace Abstract
 {
-    public class Employee<T>() : Person, IQuittable
+    public class Employee<T> : Person, IQuittable
     {
-        public int Id { get; internal set; }
+    //    public int Id { get; internal set; }
 
-        public  void SayName()
-        {
-            Console.WriteLine("Your name: ");
-        }
-    public void Quit()
-        {
-            Console.WriteLine("Your name is:");
-        }
-        //Overload the "==" operator so it checks if two Employee objects are equal by comparing their Id property.
-        public static bool operator !=(Employee a, Employee b)
-        {
-            return a.Id != b.Id;
-        }
-        public static bool operator ==(Employee a, Employee b)
-        {
-            return a.Id == b.Id;
-        }
+    //    public  void SayName()
+    //    {
+    //        Console.WriteLine("Your name: ");
+    //    }
+    //public void Quit()
+    //    {
+    //        Console.WriteLine("Your name is:");
+    //    }
+    //    //Overload the "==" operator so it checks if two Employee objects are equal by comparing their Id property.
+    //    public static bool operator !=(Employee a, Employee b)
+    //    {
+    //        return a.Id != b.Id;
+    //    }
+    //    public static bool operator ==(Employee a, Employee b)
+    //    {
+    //        return a.Id == b.Id;
+    //    }
     public List<T> Things { get; set; }
     }
 }
--- Abstract/Abstract/Program.cs
+++ Abstract/Abstract/Program.cs
@@ -11,13 +11,36 @@ namespace Abstract
         static void Main(string[] args)
         {
 
-           //IQuittable IQuitemployee = new Employee();
-            Employee employee = new Employee();
-            employee.Quit();
-            Console.ReadLine();
-            employee.SayName();
-            employee.FirstName = "sample";
-            employee.LastName = "Student";
+            //IQuittable IQuitemployee = new Employee();
+            //Employee employee = new Employee();
+            //employee.Quit();
+            //Console.ReadLine();
+            //employee.SayName();
+            //employee.FirstName = "sample";
+            //employee.LastName = "Student";
+            Employee<string> employee = new Employee<string>();
+            employee.Things = new List<string>()
+            {
+            //    List<string> Things = new List<string>(new string[] {"23", "172","239"});
+            //List<int> int1 = Things.convertAll<int>(Convert.ToInt32);
+            "Appointement",
+            "Schedule",
+            "Date",
+            "Hours"
+            };
+            Employee<int> employees = new Employee<int>();
+            employees.Things = new List<int>()
+            {
+                12, 23,45
+             };
+            foreach (string thing in employee.Things)
+            {
+                Console.WriteLine(thing);
+            }
+            foreach(int things in employees.Things)
+            {
+                Console.WriteLine(things);
+            }
             Console.ReadLine();
         }
         
