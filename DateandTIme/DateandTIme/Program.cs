e1a9a2d163e9657fec59af72dfe97c9d4cfd5d8d
moise7 <63320773+moise7@users.noreply.github.com>
2020-10-19 23:01:13 -0400

    Add files via upload

---
+++ DateandTIme/DateandTIme/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DateandTIme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now);
+            Console.ReadLine();
+            Console.WriteLine("Enter a number");
+           int num = Convert.ToInt32(Console.ReadLine());
+            DateTime d2 = now.AddHours(num);
+            Console.WriteLine("the exaxt time "+d2);
+        }
+    }
+}
