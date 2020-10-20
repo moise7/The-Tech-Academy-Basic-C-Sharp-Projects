22e4f8e3e269b3aa5c50d5ef9c0c3832c4d03b28
moise7 <63320773+moise7@users.noreply.github.com>
2020-10-19 22:00:44 -0400

    Add files via upload

---
+++ FileAssign/FileAssign/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FileAssign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Asks the user for a number.
+            Console.WriteLine("Enter a number");
+            string num = Console.ReadLine();
+            //Logs that number to a text file.
+            System.IO.File.WriteAllText(@"C:\Users\MB New Vision\Desktop\The Tech Academy\Framework\FileAssign\IO\text.txt", num);
+            string num1 = System.IO.File.ReadAllText(@"C:\Users\MB New Vision\Desktop\The Tech Academy\Framework\FileAssign\IO\text.txt");
+            Console.WriteLine(num1);
+        }
+    }
+}
