a5e9210576e654eb9ed2469dbb4cbcfe7c33987f
moise7 <63320773+moise7@users.noreply.github.com>
2020-10-21 14:47:42 -0400

    Add files via upload

--- Const/Const/Program.cs
+++ Const/Const/Program.cs
@@ -6,8 +6,21 @@ namespace Const
     {
         static void Main(string[] args)
         {
+          
             //Creating a const
             const string name = "Moise";
+            //Create a variable using the keyword "var.”
+            var i = 100;
+          
+        }
+        // Chain two constructors together.
+        public Program (): this(2)
+        {
+
+        }
+        public Program(int name)
+        {
+
         }
     }
 }
